14465ccfd1fddb9f99a7f9d3ce3b025631c2816d
Saliou <dmsaliou9292@gmail.com>
2024-05-30 17:48:08 +0200

    services done ...

--- src/main/java/org/msd/ebankingbackend/handlers/ExceptionRepresentation.java
+++ src/main/java/org/msd/ebankingbackend/handlers/ExceptionRepresentation.java
@@ -1,7 +1,6 @@
 package org.msd.ebankingbackend.handlers;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
--- src/main/java/org/msd/ebankingbackend/repositories/ContactRepository.java
+++ src/main/java/org/msd/ebankingbackend/repositories/ContactRepository.java
@@ -4,9 +4,7 @@ import org.msd.ebankingbackend.entities.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
