90dd4b1804ae78516c56de0bfafc23ed237b4232
Liousa <dmsaliou9292@gmail.com>
2024-09-23 00:40:07 +0200

    ajout du controller, service (register, authenticate), mapping avec MapStruct

--- src/main/java/org/msd/ebankingbackend/storage/mappers/AbstractEntityMapper.java
+++ src/main/java/org/msd/ebankingbackend/storage/mappers/AbstractEntityMapper.java
@@ -1,12 +1,8 @@
 package org.msd.ebankingbackend.storage.mappers;
 
-import org.mapstruct.MappingTarget;
-
 public interface AbstractEntityMapper<M, E> {
 
     M toModel(E entity);
 
     E toEntity(M model);
-
-    M populateModel(@MappingTarget M model, E entity);
 }
